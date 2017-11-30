69e6094387dab2a8a6a89e255d9e1d29301557b8
saupetit <saupetit@MTP-D320-004.DIGINAMIC.LOCAL>
2017-11-30 12:33:54 +0100

    Finishing tables creating

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -19,16 +19,15 @@ public class Main {
 				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS book(id bigserial PRIMARY KEY, "
 						+ "title varchar(255) NOT NULL, " + "author varchar(255))");
 
-				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS client(id bigserial PRIMARY KEY, " + "lastname varchar(255) NOT NULL, "
-						+ "firstname varchar(255))" + "gender varchar(255)" +"");
+				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS client(id bigserial PRIMARY KEY, "
+						+ "lastname varchar(255) NOT NULL, " + "firstname varchar(255), " + "gender varchar(255), "
+						+ "id_favourite_book INTEGER CONSTRAINT fk_id_fav_book REFERENCES book(id))");
 
-				/*
-				 * stmt.
-				 * executeUpdate("INSERT INTO book(title, author) VALUES('What''s New in Java 8', 'Adam L. Davis')"
-				 * ); stmt.executeUpdate(
-				 * "INSERT INTO book(title, author) VALUES('Welcome to Java for Python Programmers', 'Brad Miller')"
-				 * );
-				 */
+				stmt.executeUpdate(
+						"CREATE TABLE IF NOT EXISTS prefer (id_book INTEGER CONSTRAINT fk_id_book REFERENCES book(id), "
+								+ "id_client INTEGER CONSTRAINT fk_id_client REFERENCES client(id), "
+								+ "PRIMARY KEY (id_book, id_client))");
+
 				conn.commit();
 
 			} catch (Exception e) {
@@ -39,4 +38,5 @@ public class Main {
 			}
 		}
 	}
+
 }
